897cec9447dee3b2b291ef11e48140e70f7d3321
ph-plqn <ph.plqn@proton.me>
2026-08-07 09:02:18 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""Gramps Descendants Timeline."""
+
+from .model.temporal import (
+    CertaintyLevel,
+    EvidenceStatus,
+    TemporalValue,
+    ValueOrigin,
+)
+
+__all__ = [
+    "CertaintyLevel",
+    "EvidenceStatus",
+    "TemporalValue",
+    "ValueOrigin",
+]
